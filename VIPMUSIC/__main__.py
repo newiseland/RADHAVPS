02eefc8c1a2cd16faa768755c259dc20b81d7c00
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-08-30 09:05:51 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -41,12 +41,12 @@ async def init():
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
-    LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  𝐌𝐀𝐃𝐄 𝐁𝐘 𝐒𝐀𝐑𝐊𝐀𝐑\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("VIPMUSIC").info("                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VIPMUSIC").info("                 ╔═════ஜ۩۞۩ஜ════╗\n  𝐌𝐀𝐃𝐄 𝐁𝐘 𝐒𝐀𝐑𝐊𝐀𝐑\n╚═════ஜ۩۞۩ஜ════╝")
     
 
 if __name__ == "__main__":
