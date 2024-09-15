e59d2bc0c7c56e6a9937722fb7479ddbab677f41
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-16 00:25:30 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -11,7 +11,7 @@ from ..logging import LOGGER
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("-1001645282995")
+TEST_ID = int("-1002218729251")
 
 assistants = []
 assistantids = []
